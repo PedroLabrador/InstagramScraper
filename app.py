3f9479e4b37f0227069d65f22cc9358be24d6bc1
PedroLabrador <pedrolabrador3@outlook.com>
2019-06-21 11:18:07 -0430

    renew proxies

--- app.py
+++ app.py
@@ -18,7 +18,11 @@ class InstagramScraper:
 			# Set to True, just to delete the current saved posts and request them again with more data :(
 			self.user = User(data['graphql']['user'], reset_posts)
 		except Exception as e:
-			raise e
+			if request.is_enabled_proxy():
+				request.select_proxy(status=True)
+				self.get_user_profile_request(profile_url, reset_posts)
+			else:
+				raise e
 		return self.user
 
 	def get_single_post_request(self, post_url):
@@ -28,7 +32,11 @@ class InstagramScraper:
 			data      = json.loads(response.text)
 			post      = Post(data['data']['shortcode_media'])
 		except Exception as e:
-			raise e
+			if request.is_enabled_proxy():
+				request.select_proxy(status=True)
+				self.get_single_post_request(post_url)
+			else:
+				raise e
 		return post
 
 	def get_hashtag_request(self, hashtag_url, first=''):
@@ -39,5 +47,12 @@ class InstagramScraper:
 			hashtag   = Hashtag(data['data']['hashtag'])
 			self.hashtags.append(hashtag)
 		except Exception as e:
-			raise e
+			if request.is_enabled_proxy():
+				request.select_proxy(status=True)
+				self.get_hashtag_request(hashtag_url, first)
+			else:
+				raise e
 		return hashtag
+
+	def renew_proxy_list(self, option):
+		return request.get_proxy_list(option)
